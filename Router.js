939babf96c7c11953d19a48117d96ddf40914c5c
Hung Phan <hungphan@Hungs-Mac-mini.local>
2019-05-01 04:16:02 -0700

    time tracking added

--- Router.js
+++ Router.js
@@ -1,33 +1,37 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import { Text } from 'react-native-router-flux';
 import { Scene, Router, ActionConst } from 'react-native-router-flux';
 import PatientLogin from './src/screens/patientLogin';
 import PatientSignUp from './src/screens/patientSignUp';
 import PatientProfile from './src/screens/patientProfile';
 import ShowMap from './src/screens/showMap';
+import QrScanner from './src/screens/qrScanner';
 
 
-const TabIcon = ({selected, title}) => {
+const TabIcon = ({ selected, title }) => {
     return (
-        <Text style={{color: selected ? 'red':'black'}}>{title}</Text>
+        <Text style={{ color: selected ? 'red' : 'black' }}>{title}</Text>
     );
 };
 const RouterComponent = () => {
     return (
         <Router>
             <Scene key="root" hideNavBar>
-            {/* ActionConst.RESET disables moving back feature */}
+                {/* ActionConst.RESET disables moving back feature */}
                 <Scene key="auth" type={ActionConst.RESET}>
-                    <Scene hideNavBar key="login" component={PatientLogin} 
-                        title="Please Login" initial/>
-                    <Scene key="signUp" component={PatientSignUp} title="Sign Up"/>
+                    <Scene hideNavBar key="login" component={PatientLogin}
+                        title="Please Login" initial />
+                    <Scene key="signUp" component={PatientSignUp} title="Sign Up" />
                 </Scene>
-                <Scene key="tabbar" tabs tabBarStyle={{fontSize: 100, backgroundColor: '#ffffff'}}
-                        type={ActionConst.RESET}>
-                    <Scene key="map" component={ShowMap} 
-                        title="Oncology Map" 
+                <Scene key="tabbar" tabs tabBarStyle={{ fontSize: 100, backgroundColor: '#ffffff' }}
+                    type={ActionConst.RESET}>
+                    <Scene key="map" component={ShowMap}
+                        title="Oncology Map"
                     />
-                    <Scene key="PatientProfile" component={PatientProfile} 
+                    <Scene key="qrScanner" component={QrScanner}
+                        title="QR Scanner"
+                    />
+                    <Scene key="PatientProfile" component={PatientProfile}
                         title="Profile"
                     />
                 </Scene>
---
+++ src/screens/qrScanner.js
@@ -0,0 +1,128 @@
+import React, { Component } from 'react';
+import { Alert, StyleSheet, Dimensions } from 'react-native'
+import QRCodeScanner from 'react-native-qrcode-scanner';
+import firebase from 'firebase'
+import { Actions } from 'react-native-router-flux';
+
+class QrScanner extends Component {
+
+    constructor(props){
+        super(props);
+        this.user = '';
+        this.phoneNumber = '';
+        this.start = 0;
+        this.end = 0;
+        this.timeVisited = 0;
+    }
+
+
+    componentWillMount(){
+        var self = this;
+        user = firebase.auth().currentUser;
+        console.log(user);
+        phoneNumber = user.email.split('@')[0];
+
+        console.log("im in componentWillMount");
+        console.log("phoneNumber = " + phoneNumber);
+
+        firebase.database().ref('/Durations/').orderByChild('phoneNum').equalTo(phoneNumber)
+        .once('value', function(snapshot){
+            if (snapshot.exists())
+            {
+                console.log("Patient's duration is found in duration table. No need to push another duration for this patient");
+            }
+            else {
+                console.log("Patient's duration is NOT FOUND in duration table.");
+                firebase.database().ref(`/Durations`).push({
+                    phoneNum: phoneNumber,
+                    startTime: '',
+                    endTime: '',
+                }).then((data) => {
+                    console.log("Pushing a new duration for this patient")
+                    console.log("data = " + data);
+                }).catch((error) => {
+                    console.log("error pushing new duration to the server = " + error);
+                })
+            }
+        })
+    }
+
+    onSuccess(e) {
+
+        let self = this;
+
+        console.log(e);
+        console.log("phoneNumber = " + phoneNumber);
+
+        let durationObjectId = '';
+
+        if (e.data == 'start') {
+
+            firebase.database().ref('/Durations/').orderByChild('phoneNum').equalTo(phoneNumber)
+            .once('value', function(snapshot){
+                snapshot.forEach((data) => {
+                    durationObjectId = data.key
+                })
+                // self.start data is only shown in firebase function
+                self.start = Date.now();
+                firebase.database().ref().child('/Durations/' + durationObjectId)
+                .update({
+                    startTime : self.start
+                })
+                Alert.alert(
+                    'Confirm',
+                    'Successfully scanned QR code.',
+                    [
+                        {text: 'Return to Onocology Map', onPress: () => { self.scanner.reactivate(); Actions.map(); }}
+                    ]
+                )
+            })
+        }
+        else if (e.data == 'end') {
+
+            firebase.database().ref('/Durations/').orderByChild('phoneNum').equalTo(phoneNumber)
+            .once('value', function(snapshot){
+                snapshot.forEach((data) => {
+                    durationObjectId = data.key
+                })
+                // self.end data is only shown in firebase function
+                self.end = Date.now();
+                firebase.database().ref().child('/Durations/' + durationObjectId)
+                .update({
+                    endTime : self.end
+                })
+                console.log("start = " + self.start);
+                console.log("end = " + self.end);
+                self.timeVisited = self.end - self.start;
+                console.log("timeVisited = " + self.timeVisited);
+    
+                Alert.alert(
+                    'Confirm',
+                    'You spent ' + (self.timeVisited / 1000) + ' seconds',
+                    [
+                        {text: 'Return to Onocology Map', onPress: () => {self.scanner.reactivate(); Actions.map()} }
+                    ]
+                )
+            })
+        }
+        else {
+            console.log("unrecognized qr code");
+        }
+    }
+
+    render() {
+        return (
+            <QRCodeScanner
+                ref={(node) => { this.scanner = node }}
+                onRead={this.onSuccess.bind(this)}
+                cameraStyle={{ height: Dimensions.get("window").height }}
+            />
+        );
+    }
+}
+
+const styles = StyleSheet.create({
+
+});
+
+export default QrScanner;
