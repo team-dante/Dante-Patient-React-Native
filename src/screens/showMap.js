8b41833de7cbe11fce358cfbbecbf45af81ab238
Hung Phan <hungphan@dhcp-v056-070.mobile.uci.edu>
2019-05-21 18:26:14 -0700

    need to extract doctor location from json

--- src/screens/showMap.js
+++ src/screens/showMap.js
@@ -1,15 +1,15 @@
 'use strict';
 import React, { Component } from 'react';
-import {StyleSheet, Image, ScrollView, Text, Dimensions} from 'react-native';
+import { StyleSheet, Image, ScrollView, Text, Dimensions } from 'react-native';
 import firebase from 'firebase';
-import Canvas, {Image as CanvasImage, Path2D, ImageData} from 'react-native-canvas';
+import Canvas, { Image as CanvasImage, Path2D, ImageData } from 'react-native-canvas';
 
 class ShowMap extends Component {
 
     constructor(props) {
         super(props);
         // email = phoneNumber + @email.com
-        this.state = { patientName: '', queueNum: '', queueNotFound: true, data: [] };
+        this.state = { patientName: '', queueNum: '', queueNotFound: true, locationData: [] };
         this.realTimeInterval = 0;
     }
 
@@ -20,9 +20,9 @@ class ShowMap extends Component {
         // this keyword would not work under callback fxn
         var self = this;
 
-        firebase.database().ref('/WaitingQueue').on("value", function(snapshot){
-            snapshot.forEach( (data) => {
-                if (data.key == phoneNum.toString()){
+        firebase.database().ref('/WaitingQueue').on("value", function (snapshot) {
+            snapshot.forEach((data) => {
+                if (data.key == phoneNum.toString()) {
                     console.log(data.key + ': ' + data.val())
                     self.setState({ queueNum: data.val(), queueNotFound: false })
                     if (data.val() == -1)
@@ -37,33 +37,39 @@ class ShowMap extends Component {
 
         // if the patient is still inside a room, or the whole visit is not over, clock ticking
         // even if patient closes the app and reopens again, clock will work correctly
-        this.realTimeInterval = setInterval(()=> {
+        this.realTimeInterval = setInterval(() => {
             firebase.database().ref('/PatientVisitsByDates/' + phoneNum + '/' + keyMonthDateYear)
-            .once('value', function (snapshot) {
-                snapshot.forEach((data) => {
-                    if (data.val().inSession == true) {
-                        let start = data.val().startTime;
-                        let now = Date.now();
-                        data.ref.update({
-                            endTime: now,
-                            diffTime: now - start
-                        })
-                    }
+                .once('value', function (snapshot) {
+                    snapshot.forEach((data) => {
+                        if (data.val().inSession == true) {
+                            let start = data.val().startTime;
+                            let now = Date.now();
+                            data.ref.update({
+                                endTime: now,
+                                diffTime: now - start
+                            })
+                        }
+                    });
                 });
-            });
         }, 1000);
 
         // search for the staff obj that has the same phoneNum as currentUser has
         firebase.database().ref(`/Patients`).orderByChild("patientPhoneNumber").equalTo(phoneNum)
-            .once('value', function(snapshot) {
+            .once('value', function (snapshot) {
                 let firstNameVal = '';
                 snapshot.forEach(function (data) {
                     firstNameVal = data.val().firstName;
                 });
                 console.log("line 27=" + firstNameVal)
-                self.setState( { patientName : firstNameVal } );
+                self.setState({ patientName: firstNameVal });
                 // running console.log(patientName) here would cause crash
             });
+
+        console.log("im in componentwillmount")
+        // extract doctor's location
+        firebase.database().ref('/DoctorLocation/').on('value', function (snapshot) {
+            self.setState({ locationData: snapshot.val() })
+        })
     }
 
     formattedDate(now) {
@@ -88,7 +94,7 @@ class ShowMap extends Component {
             )
         }
     }
-    
+
     componentWillUnmount() {
         clearInterval(this.realTimeInterval);
     }
@@ -104,15 +110,29 @@ class ShowMap extends Component {
         this.textY = textY;
     }
 
+    Doctor(x, y, radius, sAngle, eAngle, name, textX, textY, color) {
+        this.x = x;
+        this.y = y;
+        this.radius = radius;
+        this.sAngle = sAngle;
+        this.eAngle = eAngle;
+        this.name = name;
+        this.textX = textX;
+        this.textY = textY;
+        this.color = color;
+    }
+
     handleCanvas = (canvas) => {
+        // create the entire canvas with the width of the device and the height of the device 
+        // fill with white color
         const context = canvas.getContext('2d');
         canvas.height = Dimensions.get('window').height;
         canvas.width = Dimensions.get('window').width;
-        context.fillStyle = '#fff';
+        context.fillStyle = '#3DCEBF';
         context.fillRect(0, 0, canvas.width, canvas.height);
-        context.fill()
 
         let shapeArr = [];
+
         let rectWidth = 200;
         let rectHeight = 200;
 
@@ -120,10 +140,12 @@ class ShowMap extends Component {
         let horizontalOffset = canvas.width - rectWidth;
         let textOffset = 20;
 
-        shapeArr.push(new this.Shape(0, 0, rectWidth, rectHeight, '#4B77BE', 'Room A', 0, textOffset))
-        shapeArr.push(new this.Shape(horizontalOffset, 0, rectWidth, rectHeight, '#4B77BE', 'Room B', horizontalOffset, textOffset))
-        shapeArr.push(new this.Shape(0, rectHeight + verticalOffset , rectWidth, rectHeight, '#4B77BE', 'Room C', 0, rectHeight + verticalOffset + textOffset))
-        shapeArr.push(new this.Shape(horizontalOffset, rectHeight + verticalOffset, rectWidth, rectHeight, '#4B77BE', 'Room D', horizontalOffset, rectHeight + verticalOffset + textOffset))
+        let paddingRoomText = 10;
+
+        shapeArr.push(new this.Shape(0, 0, rectWidth, rectHeight, '#4B77BE', 'Room A', paddingRoomText, textOffset))
+        shapeArr.push(new this.Shape(horizontalOffset, 0, rectWidth, rectHeight, '#4B77BE', 'Room B', horizontalOffset + paddingRoomText, textOffset))
+        shapeArr.push(new this.Shape(0, rectHeight + verticalOffset, rectWidth, rectHeight, '#4B77BE', 'Room C', paddingRoomText, rectHeight + verticalOffset + textOffset))
+        shapeArr.push(new this.Shape(horizontalOffset, rectHeight + verticalOffset, rectWidth, rectHeight, '#4B77BE', 'Room D', horizontalOffset + paddingRoomText, rectHeight + verticalOffset + textOffset))
 
         for (let i in shapeArr) {
             context.fillStyle = shapeArr[i].fillColor
@@ -133,23 +155,63 @@ class ShowMap extends Component {
             context.fillText(shapeArr[i].text, shapeArr[i].textX, shapeArr[i].textY)
         }
 
+        // showing doctors' small circles
+        let paddingNoteCircle = 30;
+        let paddingRoomTextCircle = 20;
+        let noteCircleVerticalOffset = canvas.width + paddingNoteCircle;
+        let doctorArr = [];
+        doctorArr.push(new this.Doctor(paddingNoteCircle, noteCircleVerticalOffset, 15, 0, 2 * Math.PI,
+            "Dr. Roa", paddingNoteCircle + paddingRoomTextCircle, noteCircleVerticalOffset + 5, "yellow"));
+        doctorArr.push(new this.Doctor(paddingNoteCircle, noteCircleVerticalOffset + paddingNoteCircle + 5, 15, 0, 2 * Math.PI,
+            "Dr. Kuo", paddingNoteCircle + paddingRoomTextCircle,
+            noteCircleVerticalOffset + paddingNoteCircle + 10, "red"));
+
+        for (let i in doctorArr) {
+            context.fillStyle = doctorArr[i].color;
+            context.beginPath()
+            context.arc(doctorArr[i].x, doctorArr[i].y, doctorArr[i].radius, doctorArr[i].sAngle, doctorArr[i].eAngle, false);
+            context.closePath();
+            // only arc needs to call function fill()
+            context.fill()
+
+            context.fillStyle = "white";
+            context.fillText(doctorArr[i].name, doctorArr[i].textX, doctorArr[i].textY)
+        }
+
         let self = this;
 
-        // find out where room A is 
-        firebase.database().ref('/DoctorLocation/').on('value', function(snapshot){
-            self.setState({ data : snapshot})
-        })
-      }
+    }
+
+    addDoctorLocation() {
+
+        console.log(this.state.locationData)
+
+        let jsonData = this.state.locationData;
+        let obj_lst = [];
+
+        for (let eachDoctor in jsonData) {
+            obj_lst = [];
+            let inner_dict = {
+                doctorName: jsonData[eachDoctor]["doctorName"],
+                dotColor: jsonData[eachDoctor]["dotColor"],
+                currentRoom: jsonData[eachDoctor]["room"]
+            }
+            obj_lst.push(inner_dict)
+        }
+
+        console.log(obj_lst);
+    }
 
     render() {
         return (
             <ScrollView minimumZoomScale={1} maximumZoomScale={3} contentContainerStyle={styles.container}>
-                { this.renderPositionText() }
+                {this.renderPositionText()}
                 {/* <Image source={require("../assets/radOncMap.png")} 
                 style={styles.image}
                 resizeMode="contain">
                 </Image> */}
                 <Canvas ref={this.handleCanvas} />
+                <Canvas ref={this.addDoctorLocation.bind(this)} />
             </ScrollView>
         );
     }
@@ -158,11 +220,11 @@ class ShowMap extends Component {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        backgroundColor: "#fff",
+        backgroundColor: "#3DCEBF",
     },
     image: {
-        flex:1, 
-        height: undefined, 
+        flex: 1,
+        height: undefined,
         width: undefined
     },
     topText: {
